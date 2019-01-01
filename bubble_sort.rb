6203a46e76242e8da689b38978268ead33ea5133
Khalal Walker <khalal.walker@gmail.com>
2018-12-31 16:18:20 -0800

    updated with comments and 'unless'

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,3 +1,8 @@
+# Project 1: Bubble Sort
+# Sorting algorithms are some of the earliest that you typically get exposed to in Computer Science. It may not be immediately obvious how important they are, but it shouldn’t be hard to think of some examples where your computer needs to sort some massive datasets during everyday operations.
+
+# One of the simpler (but more processor-intensive) ways of sorting a group of items in an array is bubble sort, where each element is compared to the one next to it and they are swapped if the one on the left is larger than the one on the right. This continues until the array is eventually sorted.
+
 def bubble_sort(arr)
   iterate = arr.length - 1
   loop do
@@ -10,7 +15,7 @@ def bubble_sort(arr)
       arr[i] = temp
       swapped = true
     end
-    break if swapped == false
+    break unless swapped
   end
   arr
 end
